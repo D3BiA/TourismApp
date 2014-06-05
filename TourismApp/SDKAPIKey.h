7b3d4b5070a148f00e9b686dc02edd62c164238d
Andrea Bianco <andrea.bianco90@gmail.com>
2014-06-05 17:54:30 +0200

    Add Api Key for Google Maps SDK for iOS

---
+++ TourismApp/SDKAPIKey.h
@@ -0,0 +1,9 @@
+//
+//  SDKAPIKey.h
+//  TourismApp
+//
+//  Created by Andrea Bianco on 05/06/14.
+//  Copyright (c) 2014 Alex De Biasi. All rights reserved.
+//
+
+static NSString *const kAPIKey = @"AIzaSyCLEuFt3g0G74yUnaZgGBdmiqYmLYi7cfQ";
