398ce54bf8f99691c75f79d2a9cf5c832fb148ce
Alex De Biasi <DeBiA@MacBook-Pro-di-Alex.local>
2014-06-09 17:08:31 +0200

    Inserita schermata mappa, adesso bisogna toccare 2 volte per il percorso, appena si ha plist con coordinate e geolocalizzazione modifico l’inserimento dei punti e offusco un po’ il codice che ora è copia ed incollato

---
+++ TourismApp/TAMapsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TAMapsViewController.h
+//  TourismApp
+//
+//  Created by Alex De Biasi on 09/06/14.
+//  Copyright (c) 2014 Alex De Biasi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAMapsViewController : UIViewController
+
+@end
